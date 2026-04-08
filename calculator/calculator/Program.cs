14a000e3b8ee5ab0a65bd5ff973547a1f46dbe80
nnvndra <gattusonovendra24@gmail.com>
2026-04-08 22:20:36 +0700

    update kode kalkulator

--- calculator/calculator/Program.cs
+++ calculator/calculator/Program.cs
@@ -1,7 +1,26 @@
-﻿class calculator
+﻿int a = 10, b = 11;
+
+static int Penjumlahan(int a, int b)
 {
-    static void Main(string[] args)
-    {
-       
-    }
+    return a + b;
 }
+
+static int Pengurangan(int a, int b)
+{
+    return a - b;
+}
+
+static int Perkalian(int a, int b)
+{
+    return a * b;
+}
+
+static int Pembagian(int a, int b)
+{
+    return a / b;
+}
+
+Console.WriteLine(a + " + " + b + " = " + Penjumlahan(a, b));
+Console.WriteLine(a + " - " + b + " = " + Pengurangan(a, b));
+Console.WriteLine(a + " * " + b + " = " + Perkalian(a, b));
+Console.WriteLine(a + " / " + b + " = " + Pembagian(a, b));
